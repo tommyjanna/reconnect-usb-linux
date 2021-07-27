5db96616820b064609b4c4b02cf592f9024e9df9
tommyjanna <25873341+tommyjanna@users.noreply.github.com>
2021-07-27 15:51:46 -0400

    Interface script with C program

--- reconnect-usb.sh
+++ reconnect-usb.sh
@@ -2,8 +2,8 @@
 
 # reconnect-usb.sh
 # Simple utility for that resets a USB device connected
-# to the system using IOCTL_USBFS_RESET. I use this program
-# when my external speakers are unable to play audio.
+# to the system. I use this program when my external
+# speakers are unable to play audio.
 #
 # Tommy Janna, 2021
 
@@ -15,7 +15,6 @@ for (( i=0; i < ${#deviceList[@]}; i++ ))
 do
     echo "${i})" $(echo ${deviceList[i]} | cut -d " " -f7-)
     busDevNums[$i]=/dev/bus/usb/$(echo ${deviceList[i]} | awk -F 'Bus ' '{print $2}' | cut -d ' ' -f 1)/$(echo ${deviceList[i]} | awk -F 'Device ' '{print $2}' | cut -d ' ' -f 1 | sed -r 's/(.*):/\1/') 
-
 done
 
 read -p $'\nEnter the number of the device you would like to reconnect (-1 to exit)\n>: ' deviceSelected
@@ -24,8 +23,14 @@ if [[ $deviceSelected =~ ^[0-9]+$ ]]
 then
     if (( $deviceSelected < ${#deviceList[@]} ))
     then
-        echo "Working on ${busDevNums[$deviceSelected]}"
-        # Do stuff here
+        sudo ./ioctl-reset ${busDevNums[$deviceSelected]}
+
+        if [ $? = 0 ]
+        then
+            echo "Device reset sucessfully!"
+        else
+            echo "Device reset failed!"
+        fi
     else
         echo "$0: selection out of bounds" >&2
     fi
